b359fb8bb3d1c5bdafa4b883511344f59fa78f0e
Hrutik S <134339154+HrutikS@users.noreply.github.com>
2023-06-23 02:16:20 +0530

    Update LongestCommonPrefix.java

--- Longest Common Prefix/LongestCommonPrefix.java
+++ Longest Common Prefix/LongestCommonPrefix.java
@@ -1,16 +1,36 @@
-class Solution {
-    public String longestCommonPrefix(String[] strs) {
-       Arrays.sort(strs);
-       String str1=strs[0];
-       String str2=strs[strs.length-1];
-       int i=0;
-       while(i<=str1.length()-1){
-           if(str1.charAt(i) == str2.charAt(i)){
-               i++;
-           }else{
-               break;
-           }
-       }
-       return i==0?"":str1.substring(0,i);
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array of Strings");
+        //Setting s.length()=3 for easier and quick inputs
+        String[] s = new String[3];
+        for (int i = 0; i < s.length; i++) {
+            s[i]=sc.nextLine();
+        }
+        System.out.println(longestCommonPrefix(s));
+        sc.close();
+        
+    }
+    
+    public static String longestCommonPrefix(String[] s){
+
+        Arrays.sort(s);
+        String s1 = s[0];
+        String s2 = s[s.length-1];
+        int i=0;
+        while(i<=s1.length()-1) {
+            if(s1.charAt(i) == s2.charAt(i)){
+                i++;
+            }else{
+                break;
+            }
+        }
+
+        //return (if i=0 then "" else s1.substring(0,i))
+        return i==0?"":s1.substring(0, i);
     }
 }
